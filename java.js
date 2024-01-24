0cd880d4218e624d540798f899b9c087584d287a
Shahaman0 <155882158+Shahaman0@users.noreply.github.com>
2024-01-25 00:50:01 +0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,19 @@
+let age = 20; 
+let isStudent = true; 
+
+console.log("Age:", age);
+console.log("Is Student:", isStudent);
+window.onload = function() {
+        alert("Welcome Sir!\n\nThanks for vising our website! we are offer all kinds of services as per your choice.");
+     };
+
+     let length = prompt("Enter a length in cm ..");
+     let Width = prompt("Enter a Width in cm ..");
+     const Area = length * Width ;
+      console.log(Area+"cm")
+
+      window.onload = function() 
+      {
+             let userName = prompt("What is your name?");
+             alert("Hello," + userName + "!\n\nWelcome to our website.");
+         };
